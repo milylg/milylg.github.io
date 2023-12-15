5ce8c7e399d83e7a53d2b678e8eea6efcfb080b7
milylg <2998769114@qq.com>
2023-12-15 12:36:58 +0800

    automatic push github

--- automatic push github.bat
+++ automatic push github.bat
@@ -2,4 +2,4 @@
 :: -c 设置启动参数，每个启动参数使用&&分隔
 :: "" 用于执行计算机空白字符
 :: ;bash 防止git-bash窗口关闭的结尾符号
-start C:\Program" "Files\Git\git-bash.exe -c "git add .&&git commit -m 'automatic push origin'&&git push origin main;bash"
+start C:\Program" "Files\Git\git-bash.exe -c "git add .&&git commit -m 'automatic push github'&&git push origin main;bash"
